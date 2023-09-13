1fb1ccdbceb6cb1b4480052970c763aac9832698
Jeff Kimbrel <jakpot@gmail.com>
2023-09-13 11:29:43 -0700

    added gradient validation function added (empty) readme

---
+++ R/gradient_pos_density_validation.R
@@ -0,0 +1,26 @@
+#' Check the validity of a density values
+#'
+#' This is a helper function.
+#'
+#' @param gradient_pos_density A string or vector of density values
+#' @param high A high limit for valid density values
+#' @param low A low limit for valid density values
+#'
+#' @return A TRUE boolean if the density values are valid, or a printed error
+#'
+
+gradient_pos_density_validation = function(gradient_pos_density, low = 1.55, high = 1.8) {
+  # returns TRUE is the densities are valid, and FALSE if there is a problem density
+
+  if (is.numeric(gradient_pos_density)) {
+    if (any(gradient_pos_density > high)) {
+      message(glue::glue(crayon::red("Some gradient_pos_density values are higher than {high}")))
+    } else if (any(gradient_pos_density < low)) {
+      message(glue::glue(crayon::red("Some gradient_pos_density values are lower than {low}")))
+    } else {
+      return(TRUE)
+    }
+  } else {
+    message(crayon::red("some gradient_pos_density values are non-numeric"))
+  }
+}
--- R/isotope_validation.R
+++ R/isotope_validation.R
@@ -10,7 +10,6 @@
 
 
 isotope_validation = function(isotope, isotope_list = c("12C", "13C", "14N", "15N", "16O", "18O")) {
-  # returns TRUE if it passes validation, and FALSE if not.
   # the isotope_list may change if isotopolog_label stays a thing. Only the "heavy" isotopes will be allowed.
 
   if (length(setdiff(isotope, isotope_list)) == 0) {
