f00cbde55d082cc8fe38c73f9c21c424fffb7b80
Jeff Kimbrel <jakpot@gmail.com>
2023-10-03 10:57:55 -0700

    add calculating tube_rel_abundance table to constructor

--- R/qsip_data.R
+++ R/qsip_data.R
@@ -22,6 +22,7 @@ qsip_data <- S7::new_class(
     source_data = S7::class_any,
     sample_data = S7::class_any,
     feature_data = S7::class_any,
+    tube_rel_abundance = S7::class_data.frame,
     filtered_feature_data = S7::class_data.frame,
     wads = S7::class_data.frame,
     boots = S7::class_data.frame,
@@ -31,10 +32,30 @@ qsip_data <- S7::new_class(
                          sample_data,
                          feature_data) {
 
+    # calculate tube level relative abundances
+    tube_rel_abundance = feature_data@data |> # start with raw feature data
+      pivot_longer(cols = c(everything(), -feature_id), # pivot longer
+                   names_to = "sample_id",
+                   values_to = "raw_abundance") |>
+      filter(raw_abundance > 0) |> # remove features with no abundance
+      group_by(sample_id) |> # group to calculate per-sample relative abundance
+      mutate(rel_abundance = raw_abundance / sum(raw_abundance)) |> # do the calculation
+      ungroup() |>
+      left_join(sample_data@data, by = "sample_id") %>% # add sample data to get the source_mat_id
+      filter(!is.na(source_mat_id)) |> # remove features that do not have a source id (this removes features found in the feature table but not the metadata)
+      left_join(source_data@data, by = "source_mat_id") |> # combine
+      select(feature_id, sample_id, rel_abundance, source_mat_id, gradient_pos_density, gradient_pos_rel_amt, isotope) %>%
+      group_by(feature_id, source_mat_id, isotope) |>
+      mutate(tube_rel_abundance = rel_abundance * gradient_pos_rel_amt) |> # takes the sample-adjusted abundances and gets the source-adjusted abundances
+      ungroup() |>
+      select(feature_id, sample_id, tube_rel_abundance) |>
+      pivot_wider(names_from = sample_id, values_from = tube_rel_abundance, values_fill = 0)
+
     S7::new_object(S7::S7_object(),
                    source_data = source_data,
                    sample_data = sample_data,
                    feature_data = feature_data,
+                   tube_rel_abundance = tube_rel_abundance,
                    filtered_feature_data = data.frame(),
                    wads = data.frame(),
                    boots = data.frame(),
