7c2d45a1fae49970e696a7855a7fa1f71d85fe60
Jeff Kimbrel <jakpot@gmail.com>
2023-10-08 14:49:52 -0700

    code for resampling

---
+++ R/resample_wads.R
@@ -0,0 +1,38 @@
+#' Internal function for resampling WAD values
+#'
+#' Designed as a purrr::map() function called by run_resampling.R
+#'
+#' @param i (*integer*) The specific iteration of the resampling
+#' @param wad_data (*dataframe*) A WAD dataframe to resample columns from
+#' @param type (*string*) Text for whether the wad data is from labeled or unlabeled data
+#'
+#' @returns The resampling data that will be boot in `@resamples`
+#'
+#' @export
+
+resample_wads = function(i, wad_data, type) {
+
+  # make a new names vector to abstract away the real names into a numbered list of names
+  new_names = c('feature_id', paste(type, seq(1:(ncol(wad_data))), sep = "_"))
+
+  wad_data_resampled = wad_data[,sample(ncol(wad_data), replace = T, size = ncol(wad_data)), drop = FALSE]
+
+  # double check the dimensions remain the same
+  if (identical(dim(wad_data), dim(wad_data_resampled)) == FALSE) {
+    stop("something went wrong with resampling...")
+  }
+
+  # save the original names, in case they are needed later
+  wad_data_resampled_names = colnames(wad_data_resampled)
+
+  # bring the feature id from the rownames back to a column, repair the names,
+  # add the resample # and the type, and arrange just for fun
+  wad_data_resampled = wad_data_resampled |>
+    tibble::rownames_to_column('feature_id') |>
+    tibble::as_tibble(.name_repair = ~new_names) |>
+    dplyr::mutate(resample = i, .after = feature_id) |>
+    dplyr::mutate(type = type, .after = "feature_id") |>
+    dplyr::arrange(feature_id)
+
+  wad_data_resampled
+}
---
+++ R/run_resampling.R
@@ -0,0 +1,58 @@
+#' Resample WAD values
+#'
+#' Takes a filtered WAD dataframe and resamples x times
+#'
+#' This function returns a list of resampled dataframes of x length for both the
+#' labeled and unlabeled sources.
+#'
+#' @param qsip_data_object (*qsip_data*) A qsip data object that has been filtered
+#' @param resamples (*integer*) The number of resamples/bootstraps to run
+#' @param with_seed (*integer*) An optional seed for reproducibility
+#'
+#' @returns A new `qsip_data` object with the `@resamples` slot populated with resamples wad values
+#'
+#' @export
+
+run_resampling = function(qsip_data_object,
+                         resamples = 1000,
+                         with_seed = NULL) {
+
+  if (length(qsip_data_object@filtered_wad_data) == 0) {
+    stop("ERROR: this function requires a qsip object that has been run through filter_features() ")
+  }
+
+
+
+
+  unlabeled = qsip_data_object@filter_results$unlabeled_source_mat_ids
+  labeled = qsip_data_object@filter_results$labeled_source_mat_ids
+
+
+  # set seed if given
+  if(!is.null(with_seed)){set.seed(with_seed)}
+
+  # create individual dataframes for resampling
+  labeled_wads = qsip_data_object@filtered_wad_data |>
+    tibble::column_to_rownames("feature_id") |>
+    dplyr::select(dplyr::all_of(labeled))
+
+  unlabeled_wads = qsip_data_object@filtered_wad_data |>
+    tibble::column_to_rownames("feature_id") |>
+    dplyr::select(dplyr::all_of(unlabeled))
+
+  # remove NAs?
+
+  # run resampling. This will return a list of x size with resampling results
+  labeled_resamples = purrr::map(1:resamples, \(i) resample_wads(i, labeled_wads,
+                                                                   "labeled"), .progress = "labeled resamples...")
+  unlabeled_resamples = purrr::map(1:resamples, \(i) resample_wads(i, unlabeled_wads,
+                                                                     "unlabeled"), .progress = "unlabeled resamples...")
+
+  # merge two lists into a list of lists in the @resamples slot
+  qsip_data_object@resamples = list("l" = labeled_resamples,
+                                    "u" = unlabeled_resamples)
+
+  # return the complete qsip_data object
+  qsip_data_object
+
+}
