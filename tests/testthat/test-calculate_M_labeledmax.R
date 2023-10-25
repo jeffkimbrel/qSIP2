c8a2e57caf81717e9a0ca99a62e2ecc1f105a012
Jeff Kimbrel <jakpot@gmail.com>
2023-10-25 15:40:56 -0700

    prop0 tests

--- tests/testthat/test-calculate_M_labeledmax.R
+++ tests/testthat/test-calculate_M_labeledmax.R
@@ -1,5 +1,17 @@
-test_that("Values are expected", {
-  expect_equal(calculate_M_labeledmax(310, 6, "18O", propO = 0), 310)
+test_that("Values are expected for 68% GC", {
+  expect_equal(calculate_M_labeledmax(308.0283, 9.66, "13C"), 317.66373)
+  expect_equal(calculate_M_labeledmax(308.0283, 3.84, "15N"), 311.88739)
+  expect_equal(calculate_M_labeledmax(308.0283, 6, "18O"), 320.10577)
+})
+
+test_that("prop0 should only affect 18O 68% GC", {
+  expect_equal(calculate_M_labeledmax(308.0283, 9.66, "13C"),
+               calculate_M_labeledmax(308.0283, 9.66, "13C", propO = 0))
+
+  expect_equal(calculate_M_labeledmax(308.0283, 3.84, "15N"),
+               calculate_M_labeledmax(308.0283, 3.84, "15N", propO = 0))
+
+  expect_false(calculate_M_labeledmax(308.0283, 6, "18O") == calculate_M_labeledmax(308.0283, 6, "18O", propO = .75))
 })
 
 test_that("M is formatted correctly", {
