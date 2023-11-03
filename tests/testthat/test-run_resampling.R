87e5039bfd3ec8dfcd40d7446b492186a62f9446
Jeff Kimbrel <jakpot@gmail.com>
2023-11-03 15:02:29 -0700

    a few more resampling tests

--- tests/testthat/test-run_resampling.R
+++ tests/testthat/test-run_resampling.R
@@ -1,10 +1,12 @@
-test_qsip = example_qsip_object |>
-  run_feature_filter(  unlabeled_source_mat_ids = get_all_by_isotope(example_qsip_object, "12C"),
-                       labeled_source_mat_ids = c("S178", "S179", "S180"),
-                       min_unlabeled_sources = 6,
-                       min_labeled_sources = 3,
-                       min_unlabeled_fractions = 6,
-                       min_labeled_fractions = 6)
+test_qsip <- example_qsip_object |>
+  run_feature_filter(
+    unlabeled_source_mat_ids = get_all_by_isotope(example_qsip_object, "12C"),
+    labeled_source_mat_ids = c("S178", "S179", "S180"),
+    min_unlabeled_sources = 6,
+    min_labeled_sources = 3,
+    min_unlabeled_fractions = 6,
+    min_labeled_fractions = 6
+  )
 
 test_that("works correctly", {
   expect_snapshot(run_resampling(test_qsip, resamples = 100, with_seed = 21))
@@ -17,3 +19,13 @@ test_that("wrong input types fail", {
   expect_error(run_resampling(test_qsip, resamples = "not_a_number"), "resamples should be class <numeric>")
   expect_error(run_resampling(test_qsip, progress = "not_an_option"), "progress must be either TRUE of FALSE")
 })
+
+test_that("same seed gives same result", {
+  expect_identical(run_resampling(test_qsip, resamples = 10, with_seed = 22),
+                   run_resampling(test_qsip, resamples = 10, with_seed = 22))
+})
+
+test_that("reample number is correct", {
+  expect_equal(length(run_resampling(test_qsip, resamples = 10, with_seed = 22)@resamples$u), 10)
+  expect_equal(length(run_resampling(test_qsip, resamples = 10, with_seed = 22)@resamples$l), 10)})
+
