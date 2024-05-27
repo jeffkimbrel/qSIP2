93d1d1ab493ae444de6a53e538564310400a18e9
Jeff Kimbrel <jakpot@gmail.com>
2024-05-27 12:29:22 -0700

    beta version of multi-qsip

--- R/qsip_wrapper.R
+++ R/qsip_wrapper.R
@@ -1,15 +1,18 @@
+#' Wrapper for running the qSIP EAF workflow
 #'
-#'
+#' @export
 
-
-run_qsip_wrapper = function(qsip_data_object,
+multi_qsip_wrapper = function(qsip_data_object,
                             group = NULL,
                             unlabeled_source_mat_ids,
                             labeled_source_mat_ids,
                             min_unlabeled_sources = 2,
                             min_labeled_sources = 2,
                             min_unlabeled_fractions = 2,
-                            min_labeled_fractions = 2
+                            min_labeled_fractions = 2,
+                            allow_failures = FALSE,
+                            resamples = 1000,
+                            seed = NULL
 ) {
 
 
@@ -24,11 +27,74 @@ run_qsip_wrapper = function(qsip_data_object,
                      min_labeled_fractions = min_labeled_fractions,
                      quiet = TRUE
   ) |>
-    run_resampling(allow_failures = TRUE,
-                   resamples = 1000,
-                   with_seed = 17,
+    run_resampling(allow_failures = allow_failures,
+                   resamples = resamples,
+                   with_seed = seed,
                    progress = FALSE,
                    quiet = TRUE) |>
     run_EAF_calculations()
 
+}
+
+
+#' Internal function for launching multi_qsip_wrapper()
+#'
+#' @export
+
+multi_qsip_wrapper_launcher <- function(group,
+                               name,
+                               qsip_data_object,
+                               min_unlabeled_sources = 2,
+                               min_labeled_sources = 2,
+                               min_unlabeled_fractions = 2,
+                               min_labeled_fractions = 2,
+                               allow_failures = FALSE,
+                               resamples = 1000,
+                               seed = NULL) {
+
+  # get the unlabeled and labeled source_mat_ids, split by comma into vectors, and remove whitespace
+  unlabeled = group$unlabeled |> str_split(",") |> unlist() |> str_trim()
+  labeled = group$labeled |> str_split(",") |> unlist() |> str_trim()
+
+  # if seed is in the group dataframe, use it
+  if ("seed" %in% colnames(group)) {
+    seed = pull(group, seed)
+  }
+
+  # if min_XXX_sources is in the group dataframe, use it
+  if ("min_unlabeled_sources" %in% colnames(group)) {
+    min_unlabeled_sources = pull(group, min_unlabeled_sources)
+  }
+  if ("min_labeled_sources" %in% colnames(group)) {
+    min_labeled_sources = pull(group, min_labeled_sources)
+  }
+
+  # if min_XXX_fractions is in the group dataframe, use it
+  if ("min_unlabeled_fractions" %in% colnames(group)) {
+    min_unlabeled_fractions = pull(group, min_unlabeled_fractions)
+  }
+  if ("min_labeled_fractions" %in% colnames(group)) {
+    min_labeled_fractions = pull(group, min_labeled_fractions)
+  }
+
+  if ("allow_failures" %in% colnames(group)) {
+    allow_failures = as.logical(pull(group, allow_failures))
+  }
+
+  if ("resamples" %in% colnames(group)) {
+    resamples = as.integer(pull(group, resamples))
+  }
+
+  multi_qsip_wrapper(qsip_data_object,
+                   group = name,
+                   unlabeled_source_mat_ids = unlabeled,
+                   labeled_source_mat_ids = labeled,
+                   min_unlabeled_sources = min_unlabeled_sources,
+                   min_labeled_sources = min_labeled_sources,
+                   min_unlabeled_fractions = min_unlabeled_fractions,
+                   min_labeled_fractions = min_labeled_fractions,
+                   allow_failures = allow_failures,
+                   resamples = resamples,
+                   seed = seed
+  )
 }
--- R/run_comparison_groups.R
+++ R/run_comparison_groups.R
@@ -1,13 +1,52 @@
 #' Run comparison groups
 #'
+#' This function takes a dataframe of groups and parameters and runs an entire qSIP2 EAF workflow for each group.
+#'
+#' The heart of this function is the group dataframe, which should contain the following columns:
+#' - group (required): a unique identifier for each group. This can be short, or a descriptive string describing the group
+#' - unlabeled (required): a comma-separated list of `source_mat_id`s for the unlabeled isotope. Optionally, you can use terms such as "unlabeled" or "12C" to use all `source_mat_id`s with that isotope designation
+#' - labeled (required): a comma-separated list of `source_mat_id`s for the labeled isotope
+#'
+#' Additionally, other optional columns can be included in the dataframe to set per group parameters:
+#' - min_unlabeled_sources: the minimum number of unlabeled sources required for each fraction
+#' - min_labeled_sources: the minimum number of labeled sources required for each fraction
+#' - min_unlabeled_fractions: the minimum number of unlabeled fractions required for each source
+#' - min_labeled_fractions: the minimum number of labeled fractions required for each source
+#' - allow_failures: whether to allow failures in the filtering step
+#' - resamples: the number of resamples to run
+#' - seed: the seed for the resampling
+#'
+#' The last three in the list can also be added as optional parameters, and these will override any values in the groups dataframe.
+#'
+#' @param groups A dataframe containing group, unlabeled, labeled, and other optional columns
+#' @param qsip_data_object A qsip_data object
+#' @param allow_failures Whether to allow failures in the filtering step. Sets for all groups.
+#' @param seed The seed for the resampling. Sets for all groups.
+#' @param resamples The number of resamples to run. Sets for all groups.
+#'
+#' @export
 
-run_comparison_groups <- function(groups, qsip_data_object) {
-  # groups dataframe should only contain group, unlabeled and labeled columns, and no others
-  if (!all(c("group", "unlabeled", "labeled") %in% colnames(groups))) {
-    stop("Invalid column names in groups")
+run_comparison_groups <- function(groups,
+                                  qsip_data_object,
+                                  allow_failures = NULL,
+                                  seed = NULL,
+                                  resamples = NULL) {
+
+  # check if qsip_data_object is a qsip_data object
+  if (!inherits(qsip_data_object, "qsip_data")) {
+    stop("qsip_data_object must be a <qsip_data> object")
   }
 
+  # groups dataframe should contain group, unlabeled and labeled columns, and there can be others
+  required_cols <- c("group", "unlabeled", "labeled")
+  if (!all(required_cols %in% colnames(groups))) {
+    stop(glue::glue("Missing required column names in groups dataframe: {setdiff(required_cols, colnames(groups))}"))
+  }
 
+  # groups$group column should be unique
+  if (length(unique(groups$group)) != nrow(groups)) {
+    stop("group column must be unique", call. = FALSE)
+  }
 
   # make sure all source_mat_ids in the isotope columns are in the qsip_data_object
   ## first, fill in "wildcard" source_mat_ids
@@ -29,7 +68,19 @@ run_comparison_groups <- function(groups, qsip_data_object) {
       .default = labeled
     ))
 
-  source_mat_ids_in_groups <- unlist(groups[, colnames(groups) != "group"]) |>
+  # add optional parameters to overwrite the groups dataframe, or set values if not in the groups dataframe
+  if (!is.null(allow_failures)) {
+    groups$allow_failures <- allow_failures
+  }
+  if (!is.null(seed)) {
+    groups$seed <- seed
+  }
+  if (!is.null(resamples)) {
+    groups$resamples <- resamples
+  }
+
+
+  source_mat_ids_in_groups <- unlist(groups[, colnames(groups) %in% c("labeled", "unlabeled")]) |>
     enframe() |>
     separate_rows(value, sep = ",") |>
     mutate(value = str_trim(value)) |>
@@ -40,33 +91,22 @@ run_comparison_groups <- function(groups, qsip_data_object) {
   }
 
   group_list <- groups %>%
-    pivot_longer(cols = -group, names_to = "isotope", values_to = "source_mat_ids") %>%
-    split(.$group) %>%
-    map(~ .x %>%
-      select(-group) |>
-      mutate(source_mat_ids = str_remove_all(source_mat_ids, " ")) %>%
-      mutate(source_mat_ids = str_split(source_mat_ids, ",")) |>
-      deframe())
+    split(.$group)
 
   # print the name of each element in group_list
-  o <- purrr::map2(group_list, names(group_list), \(i, j) test_func(i, j, qsip_data_object),
+  multi_qsip_list <- purrr::map2(group_list, names(group_list), \(i, j) multi_qsip_wrapper_launcher(i, j, qsip_data_object),
     .progress = list(
       type = "tasks",
       format = "Finished groups {cli::pb_bar} {cli::pb_percent}",
       clear = F
     )
   )
-  o
+
+  if (isTRUE(validate_multi_qsip(multi_qsip_list))) {
+    return(multi_qsip_list)
+  } else {
+    stop("Something went wrong with run_comparison_groups()")
+  }
 }
 
-#' Test function
 
-test_func <- function(group, name, qsip_data_object) {
-  # if group$unlabeled is "unlabeled", or an isotope, run get isotope thing. same with labeled
-
-  run_qsip_wrapper(qsip_data_object,
-    group = name,
-    unlabeled_source_mat_ids = group$unlabeled,
-    labeled_source_mat_ids = group$labeled
-  )
-}
