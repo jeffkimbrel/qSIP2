84b321267af6794d4ecc3f822393419439d125ea
Jeff Kimbrel <jakpot@gmail.com>
2024-10-10 14:01:09 -0700

    testing predictions

--- R/jgi_normalize_features.R
+++ R/jgi_normalize_features.R
@@ -9,7 +9,7 @@ jgi_normalize_features = function(features, method = "lm") {
     dplyr::left_join(samples, by = "sample_id") |>
     dplyr::select(sample_id, feature_id, COVERAGE, MIX, sequins_pg) |>
     dplyr::mutate(TYPE = ifelse(feature_id %in% jgi_mixes$feature_id, "CONTROL", "EXP")) |>
-    #filter(COVERAGE > 0) |>
+    filter(COVERAGE > 0) |> # needed to make the rownames the same in the prediction later, because it drops the NAs
     dplyr::mutate(COVERAGE = log(COVERAGE)) |> # log all data before hand
     tidyr::nest(data = -c(sample_id, TYPE)) |>
     tidyr::pivot_wider(names_from = TYPE, values_from = data)
@@ -18,8 +18,10 @@ jgi_normalize_features = function(features, method = "lm") {
 
   j2 = j |>
     dplyr::mutate(workflow = purrr::map(CONTROL, purrr::possibly(fit_regression_model, otherwise = NA), engine = method)) |>
-    dplyr::mutate(pred_control = purrr::map2(workflow, CONTROL, broom::augment),
-                  pred_experimental  = purrr::map2(workflow, EXP, broom::augment),
+    dplyr::mutate(pred_control_new = purrr::map2(workflow, CONTROL, ~broom::augment(extract_fit_engine(.x), new_data = .y, interval = "confidence")), # new_data uses data in the model, basically ignoring .y
+                  pred_experimental_new  = purrr::map2(workflow, EXP, ~broom::augment(extract_fit_engine(.x), newdata = .y, interval = "confidence")), # newdata uses data passed as .y
+                  # pred_control = purrr::map2(workflow, CONTROL, broom::augment),
+                  # pred_experimental  = purrr::map2(workflow, EXP, broom::augment),
                   model_info   = purrr::map(workflow, broom::glance),
                   coefficient_info     = purrr::map(workflow, broom::tidy))
 
